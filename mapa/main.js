5e7b4d615cebb9b2d214bead182efa9890248259
Bruno-ifpr <20251ac0080041@estudantes.ifpr.edu.br>
2026-07-18 15:03:03 -0300

    arrumei o botão para mudar o tema da pagina inicial e do mapa

--- mapa/main.js
+++ mapa/main.js
@@ -100,13 +100,13 @@ carregarPontos();document.addEventListener('DOMContentLoaded', () => {
     const temaSalvo = localStorage.getItem("tema");
     if (temaSalvo === "Escuro") {
         document.body.classList.add("Escuro");
-    }
+    } 
 
 
     const botaoMenu = document.getElementById("botaoMenu");
     const fechar = document.getElementById("closeMenu");
     
-    if (botaoMenu && fechar) {
+   if (botaoMenu && fechar) {
         botaoMenu.addEventListener("click", () => {
             document.querySelector(".escondido").classList.toggle('aberto');
         });
@@ -115,21 +115,20 @@ carregarPontos();document.addEventListener('DOMContentLoaded', () => {
         });
     }
 
-    const botaoEscuro = document.getElementById("botaoEscuro");
+    const botaoEscuro = document.getElementById("conteiner");
 
-    if (botaoEscuro) {
+     if (botaoEscuro) {
         botaoEscuro.addEventListener("click", () => {
             document.body.classList.toggle("Escuro");
-             botaoEscuro.textContent="☀️ Claro"
+           
             if (document.body.classList.contains("Escuro")) {
                 localStorage.setItem("tema","Escuro");
+                 
                 botaoEscuro.classList.add("Claro")
             }
             else {
                 localStorage.setItem("tema","Claro");
-                botaoEscuro.textContent="🌙 Escuro";
-                botaoEscuro.classList.remove("Claro")
-                
+               
             }
         });
     } 
