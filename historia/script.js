f538b5fede0c0c7ac51c87132a1ff512bb02a4ed
LuizNervous <luiz.gu.neves@gmail.com>
2026-07-29 22:57:16 -0300

    Deixando tudo mais coeso, mexendo nas pequenas coisas para ficar tudo certinho, bonitinho.

--- historia/script.js
+++ historia/script.js
@@ -1,13 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
-const botaoEscuro = document.getElementById("botaoEscuro");
+
     const temaSalvo = localStorage.getItem("tema");
     if (temaSalvo === "Escuro") {
         document.body.classList.add("Escuro");
-        if (botaoEscuro) {
-        botaoEscuro.textContent = "☀️ Claro";
-        botaoEscuro.classList.add("Claro");
-    }
-    }
+    } 
 
 
     const botaoMenu = document.getElementById("botaoMenu");
@@ -22,21 +18,20 @@ const botaoEscuro = document.getElementById("botaoEscuro");
         });
     }
 
-    
+    const botaoEscuro = document.getElementById("conteiner");
 
     if (botaoEscuro) {
         botaoEscuro.addEventListener("click", () => {
             document.body.classList.toggle("Escuro");
-             
+           
             if (document.body.classList.contains("Escuro")) {
                 localStorage.setItem("tema","Escuro");
+                 
                 botaoEscuro.classList.add("Claro")
             }
             else {
                 localStorage.setItem("tema","Claro");
-                botaoEscuro.textContent="🌙 Escuro";
-                botaoEscuro.classList.remove("Claro")
-                
+               
             }
         });
     } 
