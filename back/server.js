571e4ff67714a2e9c22c88e6e440834d4dcc9b40
LuizNervous <luiz.gu.neves@gmail.com>
2026-08-19 23:28:58 -0300

    removendo a dependencia

--- back/server.js
+++ back/server.js
@@ -3,7 +3,6 @@ const express = require('express');
 const cors = require('cors');
 const mysql = require('mysql2');
 const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
 const dns = require('dns');
 
 const app = express();
